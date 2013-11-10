349c00aff0da1da8defa8786b6a5c507a95b810b
Dragan Bjedov <draganbjedov@gmail.com>
2013-11-10 19:03:51 +0100

    Changed template file properties

--- src/draganbjedov/netbeans/csv/dataobject/package-info.java
+++ src/draganbjedov/netbeans/csv/dataobject/package-info.java
@@ -1,4 +1,8 @@
-@TemplateRegistration(folder = "Other", content = "CSV_File.csv.template")
+@TemplateRegistration(
+		folder = "Other",
+		content = "CSV_File.csv.template",
+		displayName = "CSV File",
+		requireProject = false)
 package draganbjedov.netbeans.csv.dataobject;
 
 import org.netbeans.api.templates.TemplateRegistration;
